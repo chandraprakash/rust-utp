a854a7f9b59562982d42f14e2ebff6937e3b26de
prakash <prakash@maidsafe.net>
2015-08-11 12:20:33 +0100

    updated to use udpsocket

--- examples/echo-server2/main.rs
+++ examples/echo-server2/main.rs
@@ -1,35 +1,33 @@
-extern crate utp;
 extern crate env_logger;
+extern crate net2;
 
-use utp::{UtpListener, UtpSocket};
-use std::thread;
+use net2::UdpBuilder;
 
-fn handle_client(mut s: UtpSocket) {
-    let mut buf = [0; 1500];
 
-    // Reply to a data packet with its src address, then end the connection
-    match s.recv_from(&mut buf) {
-        Ok((_, src)) => {
-            println!("<= echo public address [{:?}]", src);
-            let _ = s.send_to(src.to_string().as_bytes());
-        }
-        Err(e) => println!("{}", e)
-    }
+// A little macro to make it easier to unwrap return values or halt the program
+macro_rules! iotry {
+    ($e:expr) => (match $e { Ok(v) => v, Err(e) => panic!("{}", e), })
 }
 
 fn main() {
     // Start logger
     env_logger::init().unwrap();
 
-    // Create a listener
-    let addr = "0.0.0.0:5555";
-    let listener = UtpListener::bind(addr).unwrap();
-    println!("Started at {:?}", addr);
-    for connection in listener.incoming() {
-        // Spawn a new handler for each new connection
-        match connection {
-            Ok((socket, _src)) => { thread::spawn(move || { handle_client(socket) }); },
-            _ => ()
+    let udp_builder = iotry!(UdpBuilder::new_v4());
+    let _ = iotry!(udp_builder.reuse_address(true));
+
+    let socket = iotry!(udp_builder.bind("0.0.0.0:5555"));
+    let local_addr = iotry!(socket.local_addr());
+    println!("Started at {:?}", local_addr);
+
+    while true {
+        let mut buf = [0; 10];
+        match socket.recv_from(&mut buf) {
+            Ok((_, src)) => {
+                println!("<= echo public address [{:?}]", src);
+                let _ = socket.send_to(src.to_string().as_bytes(), &src);
+            },
+            Err(e) => println!("{}", e)
         }
     }
 }
